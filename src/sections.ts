86cc82548b09f25b264f95ecbccb6145dc86a82f
El0y-C0SM0 <eloymaciel06@gmail.com>
2023-12-31 19:26:26 -0300

    limpando arquivos do merge com os forks

--- src/sections.ts
+++ src/sections.ts
@@ -1,5 +1,3 @@
 import { SectionPatent } from "./models/sections/section-patent";
-import { SectionBrand } from "./models/sections/section-brand";
-import { SectionSoftware } from "./models/sections/section-software";
 
-export const sections = [new SectionPatent(), new SectionBrand(), new SectionSoftware()];
+export const sections = [new SectionPatent()];
